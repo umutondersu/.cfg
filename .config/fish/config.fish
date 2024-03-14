862e66088e0c5c01ac20c68def08986784b857e5
umutondersu <umutsonder@gmail.com>
2024-03-14 21:52:12 +0100

    added fish configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+set fish_greeting
+
+# Don't forget fish-add-path /usr/bin
+
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  set -x GPG_TTY (tty)
+  set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+  gpgconf --launch gpg-agent
+end
